18d6a3ae7451571a453af99aba86204583fbcbd4
EC2 Default User <ec2-user@ip-172-31-11-132.ec2.internal>
2018-02-17 12:59:00 +0000

    Setting up express server

---
+++ server/beep.py
@@ -0,0 +1,5 @@
+import sys
+from sys import argv
+print "test1\n"
+print "TESTING YAHOO"+str(sys.argv)
+sys.stdout.flush()
---
+++ server/test.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const url = require('url');
+const spawn = require('child_process').spawn;
+
+const app = express()
+app.get('/', (req, res) => {
+  let query = url.parse(req.url, true).query['test'];
+  let pythonProc = spawn('python', ["./beep.py"].concat(query));
+  pythonProc.stdout.on('data', (data)=>{
+    res.send(data.toString());
+  });
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
